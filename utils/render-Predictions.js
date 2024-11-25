8d8012f5aa6d238ed3c14c64f28885b55cc06e86
senthil <connectmsk@gmail.com>
2024-11-25 18:05:33 +0530

    Added audio

--- utils/render-Predictions.js
+++ utils/render-Predictions.js
@@ -28,7 +28,7 @@ export const renderPredictions = (predictions, ctx) => {
     ctx.fillStyle = "#000000";
     ctx.fillText(prediction.class, x, y);
     if (isPerson) {
-      // playAudio();
+      playAudio();
     }
   });
 };
